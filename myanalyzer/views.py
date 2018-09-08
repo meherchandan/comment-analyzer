54e565f6b5b32c954af014b4786e13e565bdb4a4
Meher Chandan <meherchandan12@gmail.com>
2018-09-08 19:15:10 +0400

    updated

--- myanalyzer/views.py
+++ myanalyzer/views.py
@@ -21,7 +21,7 @@ for fileid in movie_reviews.fileids('neg'):
     words = movie_reviews.words(fileid)
     neg_reviews.append((create_word_features(words), "Negative"))
   
-print(len(neg_reviews))
+print("Negative",len(neg_reviews))
 pos_reviews = []
 i =0
 for fileid in movie_reviews.fileids('pos'):
@@ -31,7 +31,7 @@ for fileid in movie_reviews.fileids('pos'):
     words = movie_reviews.words(fileid)
     pos_reviews.append((create_word_features(words), "Positive"))
      
-print(len(pos_reviews))
+print("positive",len(pos_reviews))
 train_set = neg_reviews[:80] + pos_reviews[:80]
 test_set =  neg_reviews[80:] + pos_reviews[80:]
 print(len(train_set),  len(test_set))
