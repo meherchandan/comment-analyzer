b3ed1e24698fc62dc51033d3fa5c6373cb117440
Meher Chandan <meherchandan12@gmail.com>
2018-09-08 20:43:17 +0400

    updated

--- myanalyzer/urls.py
+++ myanalyzer/urls.py
@@ -29,11 +29,11 @@ def create_word_features(words):
     useful_words = [word for word in words if word not in stopwords.words("english")]
     my_dict = dict([(word, True) for word in useful_words])
     return my_dict
-    
+
 neg_reviews = []
 i = 0
 for fileid in movie_reviews.fileids('neg'):
-    if(i>10):
+    if(i>100):
         break
     i +=1
     words = movie_reviews.words(fileid)
@@ -43,15 +43,15 @@ print("Negative",len(neg_reviews))
 pos_reviews = []
 i =0
 for fileid in movie_reviews.fileids('pos'):
-    if(i>10):
+    if(i>100):
         break
     i +=1
     words = movie_reviews.words(fileid)
     pos_reviews.append((create_word_features(words), "Positive"))
      
 print("positive",len(pos_reviews))
-train_set = neg_reviews[:8] + pos_reviews[:8]
-test_set =  neg_reviews[8:] + pos_reviews[8:]
+train_set = neg_reviews[:80] + pos_reviews[:80]
+test_set =  neg_reviews[80:] + pos_reviews[80:]
 print(len(train_set),  len(test_set))
 classifier = NaiveBayesClassifier.train(train_set)
 accuracy = nltk.classify.util.accuracy(classifier, test_set)
