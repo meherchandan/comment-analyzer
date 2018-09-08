02bf4243cfc4a253abcae306702e5e0bb71a730f
Meher Chandan <meherchandan12@gmail.com>
2018-09-08 20:23:34 +0400

    updated

--- myanalyzer/urls.py
+++ myanalyzer/urls.py
@@ -18,51 +18,12 @@ from django.conf.urls import url
 from myanalyzer.views import hello, current_datetime,hours_ahead,checkposneg
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
-import nltk.classify.util
-from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import movie_reviews
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from django.http import HttpResponse
-
-def create_word_features(words):
-    useful_words = [word for word in words if word not in stopwords.words("english")]
-    my_dict = dict([(word, True) for word in useful_words])
-    return my_dict
-
-print("views.py is running")
-neg_reviews = []
-i = 0
-for fileid in movie_reviews.fileids('neg'):
-    if(i>100):
-        break
-    i +=1
-    words = movie_reviews.words(fileid)
-    neg_reviews.append((create_word_features(words), "Negative"))
-  
-print("Negative",len(neg_reviews))
-pos_reviews = []
-i =0
-for fileid in movie_reviews.fileids('pos'):
-    if(i>100):
-        break
-    i +=1
-    words = movie_reviews.words(fileid)
-    pos_reviews.append((create_word_features(words), "Positive"))
-     
-print("positive",len(pos_reviews))
-train_set = neg_reviews[:80] + pos_reviews[:80]
-test_set =  neg_reviews[80:] + pos_reviews[80:]
-print(len(train_set),  len(test_set))
-classifier = NaiveBayesClassifier.train(train_set)
-accuracy = nltk.classify.util.accuracy(classifier, test_set)
-print(accuracy * 100)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello$', hello),
     url(r'^datetime/plus/(\d{1,2})$', hours_ahead),
     url(r'^datetime$', current_datetime),
-    url(r'^analyzedata$',checkposneg,{"classifier":classifier}),
+    url(r'^analyzedata$',checkposneg),
     url(r'^', TemplateView.as_view(template_name="index.html")),
 ]
--- myanalyzer/views.py
+++ myanalyzer/views.py
@@ -12,34 +12,33 @@ def create_word_features(words):
     my_dict = dict([(word, True) for word in useful_words])
     return my_dict
 
-# print("views.py is running")
-# neg_reviews = []
-# i = 0
-# for fileid in movie_reviews.fileids('neg'):
-#     if(i>100):
-#         break
-#     i +=1
-#     words = movie_reviews.words(fileid)
-#     neg_reviews.append((create_word_features(words), "Negative"))
+neg_reviews = []
+i = 0
+for fileid in movie_reviews.fileids('neg'):
+    if(i>10):
+        break
+    i +=1
+    words = movie_reviews.words(fileid)
+    neg_reviews.append((create_word_features(words), "Negative"))
   
-# print("Negative",len(neg_reviews))
-# pos_reviews = []
-# i =0
-# for fileid in movie_reviews.fileids('pos'):
-#     if(i>100):
-#         break
-#     i +=1
-#     words = movie_reviews.words(fileid)
-#     pos_reviews.append((create_word_features(words), "Positive"))
+print("Negative",len(neg_reviews))
+pos_reviews = []
+i =0
+for fileid in movie_reviews.fileids('pos'):
+    if(i>10):
+        break
+    i +=1
+    words = movie_reviews.words(fileid)
+    pos_reviews.append((create_word_features(words), "Positive"))
      
-# print("positive",len(pos_reviews))
-# train_set = neg_reviews[:80] + pos_reviews[:80]
-# test_set =  neg_reviews[80:] + pos_reviews[80:]
-# print(len(train_set),  len(test_set))
-# classifier = NaiveBayesClassifier.train(train_set)
-# accuracy = nltk.classify.util.accuracy(classifier, test_set)
-# print(accuracy * 100)
-def checkposneg(request,classifier):
+print("positive",len(pos_reviews))
+train_set = neg_reviews[:8] + pos_reviews[:8]
+test_set =  neg_reviews[8:] + pos_reviews[8:]
+print(len(train_set),  len(test_set))
+classifier = NaiveBayesClassifier.train(train_set)
+accuracy = nltk.classify.util.accuracy(classifier, test_set)
+print(accuracy * 100)
+def checkposneg(request):
     try:
         print("request received")
         json_data = json.loads(request.body)
